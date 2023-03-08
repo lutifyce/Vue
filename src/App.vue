a5ba79e445e3daaa22c1dd97a92dbf15e01cd958
lutifyce <lutifyce@mail.ru>
2023-03-08 23:11:15 +0300

    32

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,6 @@
 export default {
   data() {
     return {
-      hrefs: [
-			{href: '1.html', text: 'text1'},
-			{href: '2.html', text: 'text2'},
-			{href: '3.html', text: 'text3'},
-		  ],
-      products: [
-			{
-				name: 'product1',
-				price: 100,
-				quantity: 5
-			},
-			{
-				name: 'product2',
-				price: 200,
-				quantity: 4
-			},
-			{
-				name: 'product3',
-				price: 300,
-				quantity: 3
-			},
-		],
     }
   },
   methods: {
@@ -33,18 +11,7 @@ export default {
 } 
 </script>
 <template>
-  1)&nbsp;|&nbsp;
-  <ul v-for="smh in hrefs">
-    <li><a href="{{ smh.href }}">{{ smh.text }}</a></li>
-  </ul><br>
-  2)&nbsp;|&nbsp;
-  <table v-for="sale in sales">
-    <tr>
-      <td>{{ sale.name }}</td>
-      <td>{{ sale.price }}</td>
-      <td>{{ sale.quantity }}</td>
-    </tr>
-  </table>
+  <p v-for="num in 30">{{ num }}&nbsp;</p>
 </template>
 
 <style scoped>
