24ddc18dc1d4acce3c4195ef8ba86f4eee4dfc29
lutifyce <lutifyce@mail.ru>
2023-03-08 19:33:37 +0300

    5

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     data() {
 	return {
 		name: 'luti',
-		surn: 'fyce',
+		surn: 'fice',
+    text: 'page',
+		href: 'page.html',
 	}
 }
   }
@@ -14,31 +16,39 @@
 		<h1> App </h1>
 	</div>
   <br>
-  <div> <p> {{ name }} {{ surn }}</p>
+  <br>
+  <br>
+  <br>
+  <img :src="attr">
+  <div> <p> Hello, i'm {{ name }} {{ surn }}</p>
+    <p>Check out my skills <a href="page.html">here</a></p>
   </div>
 </template>
 
-<style scoped>
+<style >
 header {
   line-height: 1.5;
 }
-
+p {
+  line-height: 2.5;
+  font-size: large;
+  letter-spacing: 2px;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  color: rgb(255, 0, 0);
+}
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
-
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
-
   .logo {
     margin: 0 2rem 0 0;
   }
-
   header .wrapper {
     display: flex;
     place-items: flex-start;
