b59b94beffa4fe14d7207504c0389d0f01017036
lutifyce <lutifyce@mail.ru>
2023-03-08 22:53:16 +0300

    25

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,23 @@
 export default {
   data() {
     return {
-       den:'ee',
-       age:25,
+      visibility: true,
     }
   },
   methods: {
-     toggle: function() {
-      this.isAdmin = !this.isAdmin;
-    },
-    denOfTheWeek: function() {
-    this.den = new Date().getDay();
-    },
+   toggle: function() {
+      this.visibility = !this.visibility;
+    }
   }
 } 
 </script>
 <template>
-  <button class="button" @click="toggle">{{ isAdmin ? "админ" : "не админ" }}</button><br>
-  <p v-if="isAdmin">вы не админ</p>
-  <p v-else>ладно вы админ..  </p>
-  <br>
-  <button class="button" @click="denOfTheWeek">тык</button><br>
-  <p v-if="this.den === 1">Понедельник</p>
-  <p v-if="this.den === 2">Вторник</p>
-  <p v-if="this.den === 3">Среда</p>
-  <p v-if="this.den === 4">Четверг</p>
-  <p v-if="this.den === 5">Пятница</p>
-  <p v-if="this.den === 6">Суббота</p>
-  <p v-if="this.den === 0">Воскресенье</p>
-  <br> <p class="button" v-if="age < 14">Вы ребенок</p>
-  <p class="button"  v-else-if="age < 18">подросток</p>
-  <p class="button"  v-else-if="age < 25">молодой человек</p>
-  <p class="button"  v-else-if="age >= 26">мужчина</p>
+   <div v-if="visibility">
+  <p>+++</p>
+  <p>+++</p>
+  <p>+++</p>
+  </div>
+  <button class="button" @click="toggle">кнопкая</button>
 </template>
 
 <style scoped>
