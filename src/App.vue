b4f107cb0c3a4753c1f83a4b57ae693be906f585
lutifyce <lutifyce@mail.ru>
2023-03-15 20:59:29 +0300

    50

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,55 @@
 export default {
   data() {
      return {
-     selected: "Выберите город",
+     dayWeek: "Задание 1",
+      dayWeekOptions: [
+        "Понедельник",
+        "Вторник",
+        "Среда",
+        "Четверг",
+        "Пятница",
+        "Суббота",
+        "Воскресенье",
+      ],
+      dayYear: "",
+      dayYearOptions: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+      ],
+      monthYear: "",
+      monthYearOptions: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      yearYear: "",
+      yearYearOptions: [
+        2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012,
+        2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000,
+      ],
 	}
   },
   methods: {
-    
+    todayDate: function () {
+      var today = new Date();
+      this.dayYear = String(today.getDate());
+      this.monthYear = String(today.getMonth() + 1);
+      this.yearYear = today.getFullYear();
     },
   },
 };
 </script>
 <template>
-<select v-model="selected">
-    <option>симферополь</option>
-    <option>евпатория</option>
-    <option>ялта</option>
-    <option>симферополь</option>
+<select v-model="dayWeek">
+    <option v-for="option in dayWeekOptions">{{ option }}</option>
   </select>
-  &nbsp;
-  <p>{{ selected }}</p>
+  {{ dayWeek }}
+  <select v-model="dayYear">
+    <option v-for="option in dayYearOptions">{{ option }}</option>
+  </select>
+  <select v-model="monthYear">
+    <option v-for="option in monthYearOptions">{{ option }}</option>
+  </select>
+  <select v-model="yearYear">
+    <option v-for="option in yearYearOptions">{{ option }}</option>
+  </select>
+  <button class="button" @click="todayDate">Today's date</button>
 </template>
 
 <style scoped>
