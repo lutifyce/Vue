60ca526661ed13cfc6db314ca6539a6e4c3822c5
lutifyce <lutifyce@mail.ru>
2023-03-15 19:48:58 +0300

    44

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,60 @@
 export default {
   data() {
      return {
-      message: "Задание",
-      message1: 1,
+       num: 1,
+      num_abz: "111",
+      num_1: 0,
+      num_2: 0,
+      sum_num: "0",
+      text_1: "gfg",
+      text_2: "fgf",
+      fullName: "",
+      Name: "",
+      Surname: "",
+      Patronymic: "",
 	}
   },
   methods: {
-   
+   square: function () {
+      this.num_abz = this.num ** 2;
+    },
+    sum: function () {
+      this.sum_num = this.num_1 + this.num_2;
+    },
+    swap: function () {
+      const temp = this.text_1;
+      this.text_1 = this.text_2;
+      this.text_2 = temp;
+    },
+    split: function () {
+      this.Surname = this.fullName.split(" ")[0];
+      this.Name = this.fullName.split(" ")[1];
+      this.Patronymic = this.fullName.split(" ")[2];
     },
   },
 };
 </script>
 <template>
-  <input type="text" v-model="message" />
-<p>{{ message }}</p>
-<p>{{ message.toUpperCase() }}</p>
-<input type="number" v-model="message1" />
-<p>{{ message1 ** 2 }}</p>
+<input type="num" v-model="num" />
+  <button @click="square">Квадрат num</button>
+  &nbsp;
+  <p>{{ num_square }}</p>
+  &nbsp;
+  <input type="num" v-model="numero_uno" />
+  <input type="num" v-model="numero_dos" />
+  <button @click="sum">Сумма</button>
+  &nbsp;
+  <p>{{ sum_num }}</p>
+  &nbsp;
+  <input type="text" v-model="text_uno" />
+  <input type="text" v-model="text_dos" />
+  <button @click="swap">Замена</button>
+  &nbsp;
+  <input type="text" v-model="fullName" />
+  <button @click="split">Поделить ФИО</button>
+  <p>Фамилия = {{ userSurname }}</p>
+  <p>Имя = {{ userName }}</p>
+  <p>Отчество = {{ userPatronymic }}</p>
 </template>
 
 <style scoped>
