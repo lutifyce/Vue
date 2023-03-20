a264f3688351c362169b19dc8da0590f16b26caa
lutifyce <lutifyce@mail.ru>
2023-03-20 19:26:09 +0300

    56

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,42 @@
 export default {
   data() {
     return {
-      users: [
-			{
-				id: 1,
-				name: 'name1',
-				salary: 100,
-				age: 30,
-			},
-			{
-				id: 2,
-				name: 'name2',
-				salary: 200,
-				age: 40,
-			},
-			{
-				id: 3,
-				name: 'name3',
-				salary: 300,
-				age: 50,
-			},
-		],
+      developers: [
+        {
+          id: 1,
+          name: "name1",
+          salary: 100,
+          age: 20,
+          isEdit: false,
+        },
+        {
+          id: 2,
+          name: "name2",
+          salary: 200,
+          age: 21,
+          isEdit: false,
+        },
+        {
+          id: 3,
+          name: "name3",
+          salary: 300,
+          age: 22,
+          isEdit: false,
+        },
+      ],
     };
   },
   methods: {
-    removeValue: function (id) {
-      this.customers = this.customers.filter((customer) => {
-        return customer.id !== id;
+    removeItem: function (id) {
+      this.developers = this.developers.filter((developer) => {
+        return developer.id !== id;
       });
+    },
+    edit: function (developer) {
+      developer.isEdit = true;
+    },
+    save: function (developer) {
+      developer.isEdit = false;
     },
   },
 };
@@ -36,21 +45,31 @@ export default {
 <template>
   <table>
     <tr>
-      <th>Id</th>
-      <th>Name</th>
-      <th>Salary</th>
-      <th>Age</th>
-      <th>Remove</th>
+      <th>ID</th>
+      <th>Имя</th>
+      <th>Зарплата</th>
+      <th>Возраст</th>
     </tr>
-    <tr v-for="customer in customers">
-      <td>{{ customer.id }}</td>
-      <td>{{ customer.name }}</td>
-      <td>{{ customer.value }}</td>
-      <td>{{ customer.age }}</td>
-      <td><button class="button" @click="removeValue(customer.id)">remove</button></td>
+    <tr v-for="developer in developers">
+      <template v-if="!developer.isEdit">
+        <td>{{ developer.id }}</td>
+        <td>{{ developer.name }}</td>
+        <td>{{ developer.salary }}</td>
+        <td>{{ developer.age }}</td>
+        <td><button @click="removeItem(developer.id)">remove</button></td>
+        <td><button @click="edit(developer)">edit</button></td>
+      </template>
+      <template v-else>
+        <td><input v-model="developer.id" /></td>
+        <td><input v-model="developer.name" /></td>
+        <td><input v-model="developer.salary" /></td>
+        <td><input v-model="developer.age" /></td>
+        <td><button @click="save(developer)">save</button></td>
+      </template>
     </tr>
   </table>
 </template>
+
 
 <style scoped>
 header {
