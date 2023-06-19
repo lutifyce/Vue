8aa06481b3b602759957c2e1208628d0be815008
lutifyce <lutifyce@mail.ru>
2023-06-19 09:39:56 +0300

    63

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,26 @@ import User from './components/Employee.vue'
 export default {
   data() {
     return {
-      name: 'aaaa',
-      salary: '33333',
-      age: '12',
+      users: [
+        {
+          id: 1,
+          name: 'asd',
+          salary: 5343,
+          age:21
+        },
+        {
+          id: 2,
+          name: 'aaaa',
+          salary: 2320,
+          age: 13
+        },
+        {
+          id: 3,
+          name: 'bbbb',
+          salary: 12312123,
+          age: 81
+        },
+      ],
     }
   },
   components: {
@@ -15,7 +32,7 @@ export default {
 </script>
 
 <template>
-<User :name="name" :salary="salary" :age="age"/>
+<User v-for="user in users" :name="user.name" :salary="user.salary" :age="user.age" :key="user.id" />
 </template>
 
 
