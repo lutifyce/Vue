f72f27d055c60991d56fedd1a99a1e65c2cf445f
lutifyce <lutifyce@mail.ru>
2023-06-19 09:50:47 +0300

    68

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,45 @@ import User from './components/Employee.vue'
 export default {
   data() {
     return {
-      
+      users: [
+        {
+          id: 1,
+          name: 'Jujuba',
+          surn: 'Capybara'
+        },
+        {
+          id: 2,
+          name: 'Emran',
+          surn: 'Lox'
+        },
+        {
+          id: 3,
+          name: 'Vova',
+          surn: 'Spit'
+        },
+      ],
     }
   },
   components: {
     User
   },
   methods: {
-    userName(name) {
-      console.log(name);
-    },
+    remove(id) {
+      this.users = this.users.filter((user) => {
+        return user.id !== id;
+      })
+    }
   }
 }
 </script>
 
 <template>
-<User @show="userName"/>
+<User v-for   ="user in users"
+		:id     ="user.id"
+		:name   ="user.name"
+		:surn   ="user.surn"
+		@remove ="remove"
+		:key    ="user.id"/>
 </template>
 
 
