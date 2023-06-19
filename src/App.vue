6c6beb5a45ab907cdf9341789091715de8d95ec1
lutifyce <lutifyce@mail.ru>
2023-06-19 09:43:06 +0300

    66

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <script>
 import User from './components/Employee.vue'
-export default {
-  data() {
-    return {
-      
-    }
-  },
-  components: {
-    User
-  },
-  methods: {
-    userName(name) {
-      console.log(name);
-    },
-    userSalary(name, salary){
-console.log(name, salary);
-    }
-  }
+	export default {
+		components: {
+			User
+		},
+    methods: {
+      func(arg1, arg2) {
+		console.log(arg1, arg2);
+	}
+}
 }
 </script>
-
-<template>
-<User @show="userName" @show1="userSalary"/>
+<template >
+	<User @show="func" />
 </template>
 
-
 <style scoped>
+header {
+  line-height: 1.5;
+  
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+@media (min-width: 1024px) {
+ul
+{
+  text-transform:uppercase;
+}
+  p {
+    color: red;
+    border: '1px solid green'
+    
+}
+  .logo {
+    margin: 0 2rem 0 0;
+    
+  }
+  header .wrapper {
+    
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
