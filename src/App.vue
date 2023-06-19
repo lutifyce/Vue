94b58e067c0924011de6b04572d2c1cc5a90e6ab
lutifyce <lutifyce@mail.ru>
2023-06-19 09:41:05 +0300

    64

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,25 @@ import User from './components/Employee.vue'
 export default {
   data() {
     return {
-      users: [
-        {
-          id: 1,
-          name: 'asd',
-          salary: 5343,
-          age:21
-        },
-        {
-          id: 2,
-          name: 'aaaa',
-          salary: 2320,
-          age: 13
-        },
-        {
-          id: 3,
-          name: 'bbbb',
-          salary: 12312123,
-          age: 81
-        },
-      ],
+      
     }
   },
   components: {
     User
+  },
+  methods: {
+    asd() {
+      alert('111');
+    },
+    aret(){
+      alert('2222')
+    }
   }
 }
 </script>
 
 <template>
-<User v-for="user in users" :name="user.name" :salary="user.salary" :age="user.age" :key="user.id" />
+<User @show="asd" @show1="aret" />
 </template>
 
 
